da6b980e4a23567d9697efaa308eae2a429929b6
Sumit Chhabra <jerryjav@gmail.com>
2017-02-26 12:55:37 -0500

    Improved Lenet Architecture

--- CarND-Behavioral-Cloning-P3/model.py
+++ CarND-Behavioral-Cloning-P3/model.py
@@ -2,7 +2,9 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 lines = []
 with open('driving_log.csv') as csvfile:
@@ -12,11 +14,12 @@ with open('driving_log.csv') as csvfile:
 
 images=[]
 measurements = []
+print('read csv file')
 for line in lines:
 	source_path = line[0]
-	print(source_path)	
+	#print(source_path)	
 	filename = source_path.split('\\')[-1]
-	print(filename)
+	#print(filename)
 	current_path = 'IMG/' + filename
 	image = cv2.imread(current_path)
 	images.append(image)
@@ -28,10 +31,17 @@ print('x_train shape', x_train.shape)
 y_train = np.array(measurements)	
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Convolution2D(6, 5, 5, activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6, 5, 5, activation="relu"))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(x_train, y_train, validation_split=0.2, shuffle=True)
+model.fit(x_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=20)
 
 model.save('model.h5')
